6ce2b7cf8c454827b1f8bcfed064b840dc5fa1c0
IsaMaqueda <isamqd@gmail.com>
2020-12-01 12:19:57 -0600

    Update PlayerControls.js

--- Tarea2/PlayerControls.js
+++ Tarea2/PlayerControls.js
@@ -1,6 +1,8 @@
 //source: https://github.com/PiusNyakoojo/PlayerControls
+// Creates the Player Controls, recieves as parameters the camera, playre and the domElement
 THREE.PlayerControls = function ( camera, player, domElement ) {
 
+	//puts the parameters as variables of the constructor 
 	this.camera = camera;
 	this.player = player;
 	this.domElement = ( domElement !== undefined ) ? domElement : document;
@@ -11,21 +13,25 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	this.center = new THREE.Vector3( player.position.x, player.position.y, player.position.z );
 
+	//the speed for the movement 
 	this.moveSpeed = 0.2;
 	this.turnSpeed = 0.1;
 
+	//allows the player to zoom
 	this.userZoom = !true;
 	this.userZoomSpeed = 1.0;
 
+	//allows  the player to rotate
 	this.userRotate = true;
 	this.userRotateSpeed = 1.5;
 
+	//this makes it so the player can't rotate on it's own
 	this.autoRotate = false;
 	this.autoRotateSpeed = 0.1;
-    this.YAutoRotation = false;
+   	 this.YAutoRotation = false;
     
-    this.playerDistanceX = 1.6;
-    this.playerDistanceZ = 1.6;
+    	this.playerDistanceX = 1.6;
+   	 this.playerDistanceZ = 1.6;
 
 	this.minPolarAngle = 0;
 	this.maxPolarAngle = Math.PI;
@@ -37,18 +43,22 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
     var scope = this;
     
+	//this allows the player to use the lasers 
     var canShoot = true;
     var shootTimeout = 100;
 
+	//where the lasers are stored 
     var Lasers = []
 
 	var EPS = 0.000001;
 	var PIXELS_PER_ROUND = 1800;
 
+	//the rotation Three.js Vectors
 	var rotateStart = new THREE.Vector2();
 	var rotateEnd = new THREE.Vector2();
 	var rotateDelta = new THREE.Vector2();
 
+	//The zoom Three.js Vectors 
 	var zoomStart = new THREE.Vector2();
 	var zoomEnd = new THREE.Vector2();
 	var zoomDelta = new THREE.Vector2();
@@ -57,7 +67,9 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 	var thetaDelta = 0;
 	var scale = 1;
 
+	//creates a vector in the player position 
 	var lastPosition = new THREE.Vector3( player.position.x, player.position.y, player.position.z );
+	//starts the player in a still position 
 	var playerIsMoving = false;
 
 	var keyState = {};
@@ -68,6 +80,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	var changeEvent = { type: 'change' };
 
+	//rotates to the left, gets as a parameter the angle 
 	this.rotateLeft = function ( angle ) {
 
 		if ( angle === undefined ) {
@@ -79,7 +92,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 		thetaDelta -= angle;
 
 	};
-
+	//rotates to the right, gets as a parameter the angle 
 	this.rotateRight = function ( angle ) {
 
 		if ( angle === undefined ) {
@@ -91,7 +104,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 		thetaDelta += angle;
 
 	};
-
+	//rotates up , gets as a parameter the angle 
 	this.rotateUp = function ( angle ) {
 
 		if ( angle === undefined ) {
@@ -103,7 +116,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 		phiDelta -= angle;
 
 	};
-
+	//rotates down, gets as a parameter the angle 
 	this.rotateDown = function ( angle ) {
 
 		if ( angle === undefined ) {
@@ -116,6 +129,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	};
 
+	//Zooms the camera in, gets as a parameter the scale 
 	this.zoomIn = function ( zoomScale ) {
 
 		if ( zoomScale === undefined ) {
@@ -127,7 +141,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 		scale /= zoomScale;
 
 	};
-
+	//Zooms the camera out, gets as a parameter the scale 
 	this.zoomOut = function ( zoomScale ) {
 
 		if ( zoomScale === undefined ) {
@@ -140,6 +154,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	};
 
+	//this initializes the camera position as the same as the player 
 	this.init = function() {
 
 		this.camera.position.x = this.player.position.x + 1;
@@ -152,10 +167,13 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	this.update = function() { 
 
+		//checks which key is pressed 
 		this.checkKeyStates();
 
+		//this puts the center as the player position 
 		this.center = this.player.position;
 
+		//puts the position as the camera position 
 		var position = this.camera.position;
 		var offset = position.clone().sub( this.center );
 
@@ -177,30 +195,35 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 		phi = Math.max( EPS, Math.min( Math.PI - EPS, phi ) );
 
 		var radius = offset.length() * scale;
-
+		
+		//gets the radious 
 		radius = Math.max( this.minDistance, Math.min( this.maxDistance, radius ) );
 
+		//gets the offset of x, y ,z 
 		offset.x = radius * Math.sin( phi ) * Math.sin( theta );
 		offset.y = radius * Math.cos( phi );
 		offset.z = radius * Math.sin( phi ) * Math.cos( theta );
 
+		//if aoutorotate is true, then rotates the camera 
 		if ( this.autoRotate ) {
 
 			this.camera.position.x += this.autoRotateSpeed * ( ( this.player.position.x + this.playerDistanceX * Math.sin( this.player.rotation.y ) ) - this.camera.position.x );
 			this.camera.position.z += this.autoRotateSpeed * ( ( this.player.position.z + this.playerDistanceZ * Math.cos( this.player.rotation.y ) ) - this.camera.position.z );
 
 		} else {
-
+			//if it's false, rotates the camera as the ofset 
 			position.copy( this.center ).add( offset );
 
 		}
 
+		//rotates the camera to the center 
 		this.camera.lookAt( this.center );
 
 		thetaDelta = 0;
 		phiDelta = 0;
 		scale = 1;
-
+		
+		//checks if the player is moving or zooming 
 		if ( state === STATE.NONE && playerIsMoving ) {
 
 			this.autoRotate = true;
@@ -222,13 +245,14 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
         }
 
         playerbody.position.copy(player.position);
-        
+        //allows the lase to be controled 
         this.laserControl()
-
+	//
         this.shipPlanetCollision()
 
 	};
 
+	//this check the keys pressed 
 	this.checkKeyStates = async function () {
 
 	    if (keyState[32]) {
@@ -324,43 +348,58 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	};
 
+	//gets the rotation angle
 	function getAutoRotationAngle() { 
 
 		return 2 * Math.PI / 60 / 60 * scope.autoRotateSpeed;
 
 	}
-
+	
+	//gets the zoom scale 
 	function getZoomScale() {
 
 		return Math.pow( 0.95, scope.userZoomSpeed );
 
     }
-    
+    //shoots the lasers 
     this.shootLaser = async function ()
     {
+	    //check if the player can shoot lasers 
         if(canShoot)
         {
             canShoot = ! canShoot;
+		//creates a laser 
             let laser = new THREE.Mesh(Geometries['lzr'], Materials['lzr']);
+		// initializes the laser in the position of the player 
             laser.position.copy(this.player.position)
+		//puts the rotation of the laser the same as the player 
             laser.rotation.y = (this.player.rotation.y)
             laser.rotateX(Math.PI/2)
             laser.rotateY(Math.random()*Math.PI/4)
             laser.position.x -= 0.5 * Math.sin( this.player.rotation.y );
             laser.position.z -= 0.5 * Math.cos( this.player.rotation.y );
+		//scales the laser 
             laser.scale.set(1,5,1);
+	// adds laser to the scene 
             scene.add(laser);
+		// adds a physical body to the laser with CANNON 
             let lzrbody = addPhysicalBody(laser, {mass: 1});
+		//tags the laser 
             lzrbody.tag = 'lzr';
+		//push the lazer to the array 
             Lasers.push({mesh: laser, time: Date.now(), body: lzrbody});
+		//makes it so there is a delay between the lasers 
             await new Promise(r => setTimeout(r,shootTimeout*5));
             canShoot = ! canShoot;
         }
     }
 
+	//this function controls the lasers 
     this.laserControl = async function ()
     {
+	    
         Lasers.forEach(lzr => {
+		//deletes the lazer after some time has passed, as to not saturate the scene 
             if(lzr['time'] + 2500 < Date.now())
             {
                 scene.remove(lzr['mesh'])
@@ -369,6 +408,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
             }
             else
             {
+		    //moves the lazer in the scene 
                 lzr['mesh'].position.x -= this.moveSpeed * 2 * Math.sin( this.player.rotation.y );
                 lzr['mesh'].position.z -= this.moveSpeed * 2 * Math.cos( this.player.rotation.y );
                 lzr['body'].position.copy(lzr['mesh'].position);
@@ -376,6 +416,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
         });
     }
 
+	//
     this.shipPlanetCollision= async function()
     {
         planetArray.forEach(planet =>
@@ -492,6 +533,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
 	}*/
 
+	//creates an event when you press a key 
 	function onKeyDown( event ) {
 
     	event = event || window.event;
@@ -500,6 +542,7 @@ THREE.PlayerControls = function ( camera, player, domElement ) {
 
     }
 
+	//creates an event when you stop pressing the key 
     function onKeyUp( event ) {
 
         event = event || window.event;
